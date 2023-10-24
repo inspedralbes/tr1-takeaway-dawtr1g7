b51d27d1466050e097fb4a44ec0dc3903724dbd8
Santiago Pujol <santipujolsau@gmail.com>
2023-10-24 09:54:11 +0200

    Bàsics: models, controladors, migracions i rutes

---
+++ laravel-backend/app/Models/Comanda.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use App\Models\Llibre;
+
+class Comanda extends Model
+{
+    use HasFactory;
+
+    public function llibres()
+    {
+        return $this->belongsToMany(Llibre::class);
+    }
+}
