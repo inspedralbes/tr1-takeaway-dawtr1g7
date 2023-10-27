dad16a512bf9abf25b587fb0b52f920544b52436
Santiago Pujol <santipujolsau@gmail.com>
2023-10-27 11:59:56 +0200

    Sanctum Verification

--- laravel-backend/app/Http/Controllers/ComandesController.php
+++ laravel-backend/app/Http/Controllers/ComandesController.php
@@ -24,8 +24,12 @@ class ComandesController extends Controller
      */
     public function store(Request $request)
     {
+        $usuari = $request->user();
+        $idUsuari = $usuari->id;
+
         $comanda = new Comanda();
         $comanda->estat = 'En preparació';
+        $comanda->user_id = $idUsuari;
 
         $llibresComanda = $request->input('carrito');
         $lineesComanda = [];
