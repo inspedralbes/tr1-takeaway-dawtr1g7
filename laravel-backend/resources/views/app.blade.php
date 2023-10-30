3ede06e1f3e8275378da37df8bf5fa3eb9e93b79
akumerome.Alvaro <akumerome.alvaro_kumeniusromero@iescarlesvallbona.cat>
2023-10-30 15:38:18 +0100

    Millores generals

--- laravel-backend/resources/views/app.blade.php
+++ laravel-backend/resources/views/app.blade.php
@@ -42,6 +42,9 @@
         <li class="nav-item">
           <a class="nav-link" href="{{route('categories')}}">Categories</a>
         </li>
+        <li class="nav-item">
+          <a class="nav-link" href="{{route('comandes')}}">Comandes</a>
+        </li>
       </ul>
     </div>
   </div>
